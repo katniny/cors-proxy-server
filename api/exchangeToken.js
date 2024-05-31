f348909c30d4f6e97858010f6237a6b460d8254e
katniny <110915403+katniny@users.noreply.github.com>
2024-05-30 22:55:59 -0600

    Create exchangeToken.js

---
+++ api/exchangeToken.js
@@ -0,0 +1,36 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { code, clientId, clientSecret, redirectUri } = req.body;
+
+    const tokenEndpoint = 'https://www.patreon.com/api/oauth2/token';
+    const body = new URLSearchParams({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirectUri,
+      client_id: clientId,
+      client_secret: clientSecret
+    });
+
+    try {
+      const response = await fetch(tokenEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: body.toString(),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        res.status(200).json(data);
+      } else {
+        res.status(response.status).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
